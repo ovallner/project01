dab8d5d1d750a43ba6251c0f2d50afadb8d2b7fd
ovallner <ovallner@gmail.com>
2016-01-22 16:49:06 -0600

    Adds file in

---
+++ WordSearchSolver/wordsearch.cpp
@@ -0,0 +1,7 @@
+#include "wordsearch.h"
+
+WordSearch::WordSearch()
+{
+
+}
+
---
+++ WordSearchSolver/wordsearch.h
@@ -0,0 +1,15 @@
+#ifndef WORDSEARCH_H
+#define WORDSEARCH_H
+
+
+class WordSearch
+{
+public:
+    WordSearch();
+
+signals:
+
+public slots:
+};
+
+#endif // WORDSEARCH_H
